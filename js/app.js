8f03de56f54143dee76cd6a126aa78cc47103541
Sverre <ss0391web@gmail.com>
2026-08-15 22:52:33 +0200

    created a for loop to get out 20 different pokemons(cards) for the game

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const pokeApi = "https://pokeapi.co/api/v2/";
 
-async function fetchAPokemon() {
-  const poke = await fetch(pokeApi + "pokemon/9");
-  const data = await poke.json();
-  console.log("Data from API", data);
-  console.log("Name on the pokemon is", data.name);
+async function multiplePokemons() {
+  // Create a empty string to gather the Pokemons
+  const cards = [];
+  const numberOfPokemons = 20;
+
+  for (let i = 0; i < numberOfPokemons; i++) {
+    const randomPokemon = Math.floor(Math.random() * 151 + 1);
+    const poke = await fetch(pokeApi + `pokemon/${randomPokemon}`);
+    const data = await poke.json();
+
+    cards.push(data);
+  }
+
+  console.log("Here is your cards!", cards);
+  console.log(`Numbers of cards ${cards.length}`);
 }
 
-fetchAPokemon();
+multiplePokemons();
