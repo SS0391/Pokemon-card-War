d153a712a75eda1164f45688383df4d4e227aa23
Sverre <ss0391web@gmail.com>
2026-08-15 16:40:51 +0200

    just made a function to try and fetch a pokemon to a specific number

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 const pokeApi = "https://pokeapi.co/api/v2/";
+
+async function fetchAPokemon() {
+  const poke = await fetch(pokeApi + "pokemon/9");
+  const data = await poke.json();
+  console.log("Data from API", data);
+  console.log("Name on the pokemon is", data.name);
+}
+
+fetchAPokemon();
