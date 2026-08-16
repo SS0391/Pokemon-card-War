fb85d1fec0e1e3d43d47df64dcc4c75d41d7be21
Sverre <ss0391web@gmail.com>
2026-08-16 12:21:12 +0200

    Created empty strings for both players, so they can recive their cards(deck)
    The players can now get their cards
    
    Created the startBtn for the game

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 const pokeApi = "https://pokeapi.co/api/v2/";
 
+// Create a list of cards for both players
+let playerDeck = [];
+let computerDeck = [];
+
+// Locate the html elements to changed
+const playerCountTxt = document.querySelector("#player-count");
+const computerCountTxt = document.querySelector("#computer-count");
+const gameStatus = document.querySelector("#game-status");
+const startBtn = document.querySelector("#start-btn");
+
 async function multiplePokemons() {
+  gameStatus.textContent = "Handing out Pokemons from the API";
+  // Dactivate the button before start
+  startBtn.disabled = true;
   // Create a empty string to gather the Pokemons
   const cards = [];
   const numberOfPokemons = 20;
@@ -14,7 +27,15 @@ async function multiplePokemons() {
   }
 
   console.log("Here is your cards!", cards);
-  console.log(`Numbers of cards ${cards.length}`);
+
+  // Deal out the cards -- 20 card then split them into 10 card each
+  playerCountTxt.textContent = playerDeck.length;
+  computerCountTxt.textContent = computerDeck.lenght;
+
+  // Start btn getting prepared
+  gameStatus.textContent = "Players have their Pokemonss! Lets get ready to rumble!";
+  startBtn.textContent = "Play a round!";
+  startBtn.disabled = false;
 }
 
 multiplePokemons();
