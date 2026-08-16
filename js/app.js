06cf4de809377f3e176e33600dffde0db0a791d5
Sverre <ss0391web@gmail.com>
2026-08-16 15:35:41 +0200

    small fixes on mispelling etc

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ let computerDeck = [];
 // Locate the html elements to changed
 const playerCountTxt = document.querySelector("#player-count");
 const computerCountTxt = document.querySelector("#computer-count");
-const gameStatus = document.querySelector("#game-status");
+const gameStatus = document.querySelector("#loading");
 const startBtn = document.querySelector("#start-btn");
 
 async function multiplePokemons() {
@@ -19,6 +19,7 @@ async function multiplePokemons() {
   const numberOfPokemons = 20;
 
   for (let i = 0; i < numberOfPokemons; i++) {
+    // Only using the first 151 pokemons
     const randomPokemon = Math.floor(Math.random() * 151 + 1);
     const poke = await fetch(pokeApi + `pokemon/${randomPokemon}`);
     const data = await poke.json();
@@ -28,9 +29,11 @@ async function multiplePokemons() {
 
   console.log("Here is your cards!", cards);
 
+  playerDeck = cards.slice(0, 10);
+  computerDeck = cards.slice(10, 20);
   // Deal out the cards -- 20 card then split them into 10 card each
   playerCountTxt.textContent = playerDeck.length;
-  computerCountTxt.textContent = computerDeck.lenght;
+  computerCountTxt.textContent = computerDeck.length;
 
   // Start btn getting prepared
   gameStatus.textContent = "Players have their Pokemonss! Lets get ready to rumble!";
